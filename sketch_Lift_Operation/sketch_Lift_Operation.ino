c38b92e812efe8272a8832cc658c1c499211d482
V Bhanuprakash Reddy <vbhanu406@outlook.com>
2026-08-26 22:49:08 +0530

    Lift_Project_ESP32
    Practice project for lift operation using ESP32 Dev board.

---
+++ sketch_Lift_Operation/sketch_Lift_Operation.ino
@@ -0,0 +1,19 @@
+
+#define LED_UP_ARROW    1
+#define LED_DOWN_ARROW  2
+#define LED_BUILTIN     13
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("[setup] ...");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+
+}
