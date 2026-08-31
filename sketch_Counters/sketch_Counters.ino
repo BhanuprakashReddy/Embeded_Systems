0e01d1fe424c624f35689a7fea00eb4d7cdc076c
V Bhanuprakash Reddy <vbhanu406@outlook.com>
2026-08-31 11:06:12 +0530

    Counter implementation
    Counter logic implement along with timer to get rid of delay function calls.

---
+++ sketch_Counters/sketch_Counters.ino
@@ -0,0 +1,65 @@
+/*************************************************
+Counter implementation with help of timers
+*************************************************/
+#define LED_BUILTIN 2            // 2 PIN in ESP32, 13 Pin in Arduino
+#define DEFAULT_COUNTER_RESET 10  
+
+int nCounter = 0;
+int nSensorValue = DEFAULT_COUNTER_RESET;
+int nSumValue = 0;
+bool nFlag = false;
+
+// Timer object initialization 
+hw_timer_t *timer = NULL;
+
+// Timer callback function
+void ARDUINO_ISR_ATTR onTimer(){
+  nCounter++;
+  nFlag = true;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  if(timer == NULL){
+    timer = timerBegin(100000);
+   }
+   
+  timerAttachInterrupt(timer, &onTimer);
+  timerAlarm(timer, 100000, true, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(nCounter == (int)DEFAULT_COUNTER_RESET) {
+
+    //Serial.println("Sensor sum value: ");
+    //Serial.println(nSumValue);
+    
+    int AverageValue = nSumValue/10;
+    Serial.println("Sensor average value: ");
+    Serial.println(AverageValue);
+    
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(300);
+    nFlag = false;
+    nCounter = 0;
+    nSumValue = 0;
+    nSensorValue = DEFAULT_COUNTER_RESET;
+  }
+
+  if(nFlag)
+  {
+    nFlag = false;
+    nSumValue += nSensorValue;
+    
+    Serial.println("Sensor sum value: ");
+    Serial.println(nSumValue);
+    nSensorValue++;
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
--- sketch_Lift_Operation/sketch_Lift_Operation.ino
+++ sketch_Lift_Operation/sketch_Lift_Operation.ino
@@ -1,7 +1,8 @@
 
 #define LED_UP_ARROW    1
 #define LED_DOWN_ARROW  2
-#define LED_BUILTIN     13
+#define USER_INPUT      3
+#define LED_IN_BOARD    13
 
 enum ElevatorState {
   IDLE,
@@ -9,19 +10,35 @@ enum ElevatorState {
   DOOR_OPEN
 };
 
-ElevatorState currentState = IDLE;
+ElevatorState currentState  = IDLE;
+short int currenPosition    = 0;
+short int targetPosition    = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.println("[setup] ...");
+
+  pinMode(USER_INPUT,     INPUT);
+  pinMode(LED_UP_ARROW,   OUTPUT);
+  pinMode(LED_DOWN_ARROW, OUTPUT);
+  pinMode(LED_IN_BOARD,   OUTPUT);
+    
+  Serial.println("[setup] Done.");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
   
-
+  targetPosition = digitalRead(USER_INPUT);
+  if(targetPosition != currenPosition){
+    if(targetPosition > currenPosition){
+      digitalWrite(LED_UP_ARROW, HIGH);
+      digitalWrite(LED_DOWN_ARROW, LOW);
+    }
+    else{targetPosition < currenPosition)
+      digitalWrite(LED_DOWN_ARROW, HIGH);
+      digitalWrite(LED_UP_ARROW, LOW);
+    }
+    currenPosition = targetPosition;
+  }
 }
